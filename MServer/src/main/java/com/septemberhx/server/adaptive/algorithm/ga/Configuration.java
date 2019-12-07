2990a8d57448b216e2bb99106b5ef522cbe2235a
SeptemberHX <september_hx@outlook.com>
2019-12-07 23:16:20 +0800

    change: ga parameters

--- MServer/src/main/java/com/septemberhx/server/adaptive/algorithm/ga/Configuration.java
+++ MServer/src/main/java/com/septemberhx/server/adaptive/algorithm/ga/Configuration.java
@@ -11,14 +11,14 @@ public class Configuration {
     public static boolean VERIFY_EVERY_CHILD = false;
     public static boolean COMPOSITION_ALL_ENABLED = true;
 
-    public static int POPULATION_SIZE = 100;
+    public static int POPULATION_SIZE = 150;
     public static int MOEAD_NEIGHBOR_SIZE = 20;
 
-    public static int WSGA_MAX_ROUND = 400;
-    public static int NSGAII_MAX_ROUND = 400;
-    public static int MOEAD_MAX_ROUND = 400;
+    public static int WSGA_MAX_ROUND = 300;
+    public static int NSGAII_MAX_ROUND = 300;
+    public static int MOEAD_MAX_ROUND = 300;
 
-    public static double WSGA_MUTATION_RATE = 200;
-    public static double NSGAII_MUTATION_RATE = 200;
-    public static double MOEAD_MUTATION_RATE = 200;
+    public static double WSGA_MUTATION_RATE = 0.2;
+    public static double NSGAII_MUTATION_RATE = 0.2;
+    public static double MOEAD_MUTATION_RATE = 0.2;
 }
