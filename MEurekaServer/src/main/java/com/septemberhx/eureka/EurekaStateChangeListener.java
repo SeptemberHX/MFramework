6b603d9a34dcca98f08f21a3c80d62ab2f46f715
SeptemberHX <tianjianshan@outlook.com>
2020-08-25 15:58:14 +0800

    new version MEurekaServer for future extension

--- MEurekaServer/src/main/java/com/septemberhx/eureka/EurekaStateChangeListener.java
+++ MEurekaServer/src/main/java/com/septemberhx/eureka/EurekaStateChangeListener.java
@@ -2,8 +2,11 @@ package com.septemberhx.eureka;
 
 import com.netflix.appinfo.InstanceInfo;
 import com.netflix.discovery.EurekaClient;
+import com.netflix.discovery.shared.Application;
 import com.septemberhx.common.base.MClusterConfig;
 import com.septemberhx.common.bean.MInstanceRegisterNotifyRequest;
+import com.septemberhx.common.utils.MRequestUtils;
+import com.septemberhx.common.utils.MUrlUtils;
 import com.septemberhx.eureka.client.MClusterAgentClient;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -16,6 +19,7 @@ import org.springframework.cloud.netflix.eureka.server.event.EurekaRegistryAvail
 import org.springframework.cloud.netflix.eureka.server.event.EurekaServerStartedEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Component
 public class EurekaStateChangeListener {
@@ -40,20 +44,37 @@ public class EurekaStateChangeListener {
 
     @EventListener
     public void listen(EurekaInstanceRegisteredEvent event) {
-        InstanceInfo instanceInfo = event.getInstanceInfo();
-        System.out.println(instanceInfo.getAppName()+"|"+instanceInfo.getInstanceId()+"|"+instanceInfo.getIPAddr()+"|"+instanceInfo.getPort());
-        if (!instanceInfo.getAppName().equalsIgnoreCase(MClusterConfig.MCLUSTERAGENT_NAME) && instanceInfo.getPort() != 0) {
-            try {
-                MInstanceRegisterNotifyRequest notifyRequest = new MInstanceRegisterNotifyRequest();
-                notifyRequest.setIp(instanceInfo.getIPAddr());
-                notifyRequest.setPort(instanceInfo.getPort());
-                notifyRequest.setInstanceInfo(instanceInfo);
-                this.clusterAgentClient.instanceRegistered(notifyRequest);
-                logger.info(instanceInfo);
-            } catch (Exception e) {
-                logger.debug(e);
-                e.printStackTrace();
-                logger.warn("Failed to connect to MClusterAgent!");
+        Application clusterAgentApp = this.eurekaClient.getApplication(MClusterConfig.MCLUSTERAGENT_NAME);
+        System.out.println("=====");
+        for (Application app : this.eurekaClient.getApplications().getRegisteredApplications()) {
+            System.out.println(app.getName());
+        }
+        System.out.println("-----");
+        if (clusterAgentApp != null) {
+            InstanceInfo clusterInfo = clusterAgentApp.getInstances().get(0);
+            InstanceInfo instanceInfo = event.getInstanceInfo();
+            System.out.println(instanceInfo.getAppName() + "|" + instanceInfo.getInstanceId() + "|" + instanceInfo.getIPAddr() + "|" + instanceInfo.getPort());
+            if (!instanceInfo.getAppName().equalsIgnoreCase(MClusterConfig.MCLUSTERAGENT_NAME) && instanceInfo.getPort() != 0) {
+                try {
+                    MInstanceRegisterNotifyRequest notifyRequest = new MInstanceRegisterNotifyRequest();
+                    notifyRequest.setIp(instanceInfo.getIPAddr());
+                    notifyRequest.setPort(instanceInfo.getPort());
+                    notifyRequest.setInstanceInfo(instanceInfo);
+                    MRequestUtils.sendRequest(
+                            MUrlUtils.getRemoteUri(
+                                    clusterInfo.getIPAddr(),
+                                    clusterInfo.getPort(),
+                                    MClusterConfig.MCLUSTERAGENT_INSTANCE_REGISTER_URL
+                            ),
+                            notifyRequest,
+                            null,
+                            RequestMethod.POST
+                    );
+                    logger.info(instanceInfo);
+                } catch (Exception e) {
+                    logger.info(e);
+                    logger.warn("Failed to connect to MClusterAgent!");
+                }
             }
         }
     }
